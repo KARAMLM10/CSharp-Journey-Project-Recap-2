b61278154eef887e6c022f94423e9f06ab513461
Karam Almakdasi <karamalmakdasi@gmail.com>
2024-12-10 23:31:36 +0100

    #- Math-Class

--- CSharp-Journey-Project-Recap-2/Program.cs
+++ CSharp-Journey-Project-Recap-2/Program.cs
@@ -33,7 +33,7 @@
 
 //Console.WriteLine(pi);
 
-int friends = 5;
+//int friends = 5;
 //friends = friends + 2;
 //friends += 2;
 //friends ++;
@@ -47,4 +47,21 @@ int friends = 5;
 
 
 
-Console.WriteLine(friends);
+//Console.WriteLine(friends);
+
+//==================================================
+// Math-Class
+
+double x = 3.99;
+double y = 5;
+
+//double a = Math.Pow(x, 2);
+//double b = Math.Sqrt(x);
+//double c = Math.Abs(x);
+//double d = Math.Round(x);
+//double e = Math.Ceiling(x);
+//double f = Math.Floor(x);
+//double g = Math.Max(x, y);
+double h = Math.Min(x, y);
+
+Console.WriteLine(h);
