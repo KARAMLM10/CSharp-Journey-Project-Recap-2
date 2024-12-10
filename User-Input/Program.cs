ab7a922dd2b9f854bb600325077d86df1b4cf510
Karam Almakdasi <karamalmakdasi@gmail.com>
2024-12-10 20:20:21 +0100

    #- User-Input

---
+++ User-Input/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("whats your name");
+string name = Console.ReadLine();
+Console.WriteLine("whats your age");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("hello : " + name);
+Console.WriteLine("your age is  : " + age);
